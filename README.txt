3c7236602e3d4389459165023d84437cb114aea3
metaphysicsIO <103212704+metaphysicsIO@users.noreply.github.com>
2023-05-02 13:19:19 -0500

    pushed to github

---
+++ README.txt
@@ -0,0 +1,18 @@
+# ASS2SRT
+
+Advanced SubStation Alpha (ASS/SSA) doesn't work well on minidlna
+and I need something to convert them to SRT files.
+
+I only have a prototype made at the moment. It was made with mksh in mind. I
+have also only tested it on one file, which it does work with.
+
+It should be fairly easy to use a loop to make batch edits.
+
+I plan to write this in C++ in the future. I will keep the prototype here
+though. It might not even have a purpose to have in C++.
+
+TODO: Create a batch option.
+
+TODO: Remove the extra lines at the bottom of the SRT
+
+TODO: Test against other files.
---
+++ a2s.sh
@@ -0,0 +1,20 @@
+
+input=$1
+
+srt=`echo $input | sed "s/ass/srt/"`
+
+
+# Get numbering
+echo 1 `grep "Dialogue:" $input | wc -l` | sed "s/^/seq /" | sh | sed "s/$/\n\n\n/" > 01-NUMBERING.txt
+
+# Timing
+grep "Dialogue:" $input | sed "s/Dialogue: 0,//" | sed "s/.Style.*.//" | sed "s/,/ --> /" | sed "s/$/\n\n\n/" | sed "1s/^/\n/" > 02-TIMING.txt
+
+# Get the text.. I think this will work.
+grep "Dialogue:" $input | sed "s/.*.0000..//" | sed "s/$/\n\n\n/" | sed "1s/^/\n\n/" > 03-DIALOG.txt
+
+# Fix weird spacing and newlines
+paste -d" " 01-NUMBERING.txt 02-TIMING.txt 03-DIALOG.txt | sed "s/^  //" | sed "s/^ //"| sed "s/\\\N/\n/" | sed "s/\\\n/\n/" > $srt
+
+# clean up environment
+rm 01-NUMBERING.txt 02-TIMING.txt 03-DIALOG.txt
