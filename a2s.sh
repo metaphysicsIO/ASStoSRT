9ea9414175d8d2978adab15b1e3e5473cfcc32fe
metaphysicsIO <103212704+metaphysicsIO@users.noreply.github.com>
2023-05-03 17:17:03 -0500

    Removed carriage-return character (^M)

--- a2s.sh
+++ a2s.sh
@@ -11,7 +11,7 @@ echo 1 `grep "Dialogue:" "$input" | wc -l` | sed "s/^/seq /" | sh | sed "s/$/\n\
 grep "Dialogue:" "$input" | sed "s/Dialogue: 0,//" | sed "s/.Style.*.//" | sed "s/,/ --> /" | sed "s/\./,/g" | sed "s/$/\n\n\n/" | sed "1s/^/\n/" > 02-TIMING.txt
 
 # Get the text.. I think this will work.
-grep "Dialogue:" "$input" | sed "s/.*.0000..//" | sed "s/$/\n\n\n/" | sed "1s/^/\n\n/" > 03-DIALOG.txt
+grep "Dialogue:" "$input" | sed "s/.*.0000..//" | sed "s/$/\n\n\n/" | sed "1s/^/\n\n/" | sed "s/\r//" > 03-DIALOG.txt
 
 # Fix weird spacing and newlines
 paste -d" " 01-NUMBERING.txt 02-TIMING.txt 03-DIALOG.txt | sed "s/^  //" | sed "s/^ //"| sed "s/\\\N/\n/" | sed "s/\\\n/\n/" > $srt
