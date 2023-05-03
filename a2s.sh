8cd72e905ee883718444d7df42e370c1252014bd
metaphysicsIO <103212704+metaphysicsIO@users.noreply.github.com>
2023-05-03 17:49:44 -0500

    Removed the extra 2 spaces at the end. This fixes the SRT issue for minidlna!

--- a2s.sh
+++ a2s.sh
@@ -5,7 +5,7 @@ srt=`echo $input | sed "s/ass/srt/"`
 
 
 # Get numbering
-echo 1 `grep "Dialogue:" "$input" | wc -l` | sed "s/^/seq /" | sh | sed "s/$/\n\n\n/" > 01-NUMBERING.txt
+echo 1 `grep "Dialogue:" "$input" | wc -l` | sed "s/^/seq /" | sh | sed "s/  //g" | sed "s/$/\n\n\n/" > 01-NUMBERING.txt
 
 # Timing
 grep "Dialogue:" "$input" | sed "s/Dialogue: 0,/0/" | sed "s/.Style.*.//" | sed "s/,/0 --> 0/" | sed "s/\./,/g" | sed "s/$/0/" | sed "s/$/\n\n\n/" | sed "1s/^/\n/" > 02-TIMING.txt
@@ -14,7 +14,8 @@ grep "Dialogue:" "$input" | sed "s/Dialogue: 0,/0/" | sed "s/.Style.*.//" | sed 
 grep "Dialogue:" "$input" | sed "s/.*.0000..//" | sed "s/$/\n\n\n/" | sed "1s/^/\n\n/" | sed "s/\r//" > 03-DIALOG.txt
 
 # Fix weird spacing and newlines
-paste -d" " 01-NUMBERING.txt 02-TIMING.txt 03-DIALOG.txt | sed "s/^  //" | sed "s/^ //"| sed "s/\\\N/\n/" | sed "s/\\\n/\n/" > $srt
+paste -d" " 01-NUMBERING.txt 02-TIMING.txt 03-DIALOG.txt | sed "s/^  //" | sed "s/^ //"| sed "s/\\\N/\n/" | sed "s/\\\n/\n/" | sed "s/  $//" > $srt
+
 
 # clean up environment
 rm 01-NUMBERING.txt 02-TIMING.txt 03-DIALOG.txt
