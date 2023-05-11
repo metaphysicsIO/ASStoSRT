25991adb822f1777144dbe8c3ae278f4ef2b0e64
metaphysicsIO <103212704+metaphysicsIO@users.noreply.github.com>
2023-05-11 18:11:38 -0500

    better method for selecting, dealt with superfluous typesetting (NOTE: May need to edit this later, I could see it potentially breaking things. May need to be more specific.)

--- a2s.sh
+++ a2s.sh
@@ -8,14 +8,18 @@ srt=`echo $input | sed "s/ass/srt/"`
 echo 1 `grep "Dialogue:" "$input" | wc -l` | sed "s/^/seq /" | sh | sed "s/  //g" | sed "s/$/\n\n\n/" > 01-NUMBERING.txt
 
 # Timing
-grep "Dialogue:" "$input" | sed "s/Dialogue: 0,/0/" | sed "s/.Style.*.//" | sed "s/,/0 --> 0/" | sed "s/\./,/g" | sed "s/$/0/" | sed "s/$/\n\n\n/" | sed "1s/^/\n/" > 02-TIMING.txt
+grep "Dialogue:" "$input" | sed "s/,/[REMOVE0]/1"  | sed "s/,/[REMOVE1]/2" | sed "s/.*.\[REMOVE0\]//" | sed "s/\[REMOVE1\].*.//" | sed "s/,/0 --> 0/" | sed "s/\./,/g" | sed "s/$/0/" | sed "s/$/\n\n\n/" | sed "1s/^/\n/" > 02-TIMING.txt
+
 
 # Get the text.. I think this will work.
-grep "Dialogue:" "$input" | sed "s/.*.0000..//" | sed "s/$/\n\n\n/" | sed "1s/^/\n\n/" | sed "s/\r//" > 03-DIALOG.txt
+grep "Dialogue:" "$input" | sed "s/,/[REMOVE]/9" | sed "s/.*.\[REMOVE\]//" | sed "s/$/\n\n\n/" | sed "1s/^/\n\n/" | sed "s/\r//" > 03-DIALOG.txt
 
-# Fix weird spacing and newlines
-paste -d" " 01-NUMBERING.txt 02-TIMING.txt 03-DIALOG.txt | sed "s/^  //" | sed "s/^ //"| sed "s/\\\N/\n/" | sed "s/\\\n/\n/" | sed "s/  $//" > $srt
-
+# Formatting issues:
+#       * Weird spacing
+#       * Convert newlines
+#       * Remove \{k values (SLOPPY TODO: Make smaller.)
+#       * Remove extra \{ values -- (This may be a poor decision. REMOVE IF NEEDED: "/{.*.}//")
+paste -d" " 01-NUMBERING.txt 02-TIMING.txt 03-DIALOG.txt | sed "s/^  //" | sed "s/^ //" | sed "s/{.k.}//g" | sed "s/{.k..}//g" | sed "s/{.k...}//g" | sed "s/{.k....}//g" | sed "s/{.*.}//" | sed "s/\\\N/\n/g" | sed "s/\\\n/\n/" | sed "s/  $//" > $srt
 
 # clean up environment
 rm 01-NUMBERING.txt 02-TIMING.txt 03-DIALOG.txt
